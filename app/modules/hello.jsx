0bd1a59b21598dd16c350a5a6298d9bd02cc1ef4
fakefish <fakefish.wjy@gmail.com>
2015-07-19 15:49:47 +0800

    增加url-loader

--- app/modules/hello.jsx
+++ app/modules/hello.jsx
@@ -5,6 +5,11 @@ import '../css/b.less';
 
 export default class Hello extends React.Component {
 	render() {
-		return <h1>Hello from Component Hello</h1>
+		return (
+			<div>
+				<div className="img" />
+				<h1>Hello from Component Hello</h1>
+			</div>
+		)
 	}
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,30 +3,36 @@ var node_modules = path.resolve(__dirname, 'node_modules');
 var pathToReact = path.resolve(node_modules, 'react/dist/react.min.js');
 
 module.exports = {
-    entry: ['webpack/hot/dev-server',path.resolve(__dirname, 'app/main.js')],
-    resolve: {
-    	alias: {
-    		'react': pathToReact,
-    	}
-    },
-    output: {
-        path: path.resolve(__dirname, 'build'),
-        filename: 'bundle.js',
-    },
-    module: {
-    	loaders: [{
-    		test: /\.jsx?$/,
-    		loader: 'babel'
-    	}, {
-    		test: /\.css$/,
-    		loader: 'style!css'
-    	}, {
-    		test: /\.s(a|c)ss$/,
-    		loader: 'style!css!sass',
-    	}, {
-    		test: /\.less$/,
-    		loader: 'style!css!less',
-    	}],
-    	noParse: [pathToReact]
-    }
+		entry: ['webpack/hot/dev-server',path.resolve(__dirname, 'app/main.js')],
+		resolve: {
+			alias: {
+				'react': pathToReact,
+			}
+		},
+		output: {
+				path: path.resolve(__dirname, 'build'),
+				filename: 'bundle.js',
+		},
+		module: {
+			loaders: [{
+				test: /\.jsx?$/,
+				loader: 'babel'
+			}, {
+				test: /\.css$/,
+				loader: 'style!css'
+			}, {
+				test: /\.s(a|c)ss$/,
+				loader: 'style!css!sass',
+			}, {
+				test: /\.less$/,
+				loader: 'style!css!less',
+			}, {
+				test: /\.(png|jpg)$/,
+				loader: 'url?limit=25000',
+			}, {
+				test: /\.woff$/,
+				loader: 'url?limit=100000'
+			}],
+			noParse: [pathToReact]
+		}
 };
